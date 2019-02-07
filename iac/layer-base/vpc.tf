7dc4e6a031d3120be16b4cbc1e0e25e883e6b25f
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2019-02-07 12:51:58 +0100

    read the doc seems to be a good idea ^^

--- iac/layer-base/vpc.tf
+++ iac/layer-base/vpc.tf
@@ -239,7 +239,10 @@ resource "aws_route_table_association" "demo_vpc_rta_private_c" {
 
 resource "aws_route53_zone" "demo_private_zone" {
   name   = "${var.private_dns_zone}"
-  vpc = "${aws_vpc.demo_vpc.id}"
+  
+  vpc {
+    vpc_id = "${aws_vpc.demo_vpc.id}"
+  }
 
   tags {
     Environment = "private_hosted_zone"
